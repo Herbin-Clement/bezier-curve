d513453ab4736bf49af3dc01ecd0746c6dbd6038
herbin <clement.herbin.etu@univ-lille.fr>
2021-08-31 21:09:12 +0200

    add cubic bezier curve

---
+++ lib.js
@@ -0,0 +1,59 @@
+const linearInterpolation = (p0, p1, t) => {
+    // let Px = (1-t) * p0.x + t * p1.x;
+    // let Py = (1-t) * p0.y + t * p1.y;
+    p0 = p0.copy();
+    p1 = p1.copy();
+    p0.mult(1 - t);
+    p1.mult(t);
+    p0.add(p1);
+    return p0;
+}
+
+const middleOfLine = (P0, P1) => {
+    Px = (P0.x + P1.x) / 2; 
+    Py = (P0.y + P1.y) / 2; 
+    return {x: Px, y: Py};
+}
+
+// const BernSteinPolForm = (P, t) => { marche pas truc de vecteur de merde
+//     let copyArr = [];
+//     P.forEach(e => copyArr.push(e.copy()));
+//     p0 = copyArr[0].mult(- Math.pow(t, 3) + Math.pow(t, 2) - 3 * t + 1);
+//     p1 = copyArr[1].mult(3 * Math.pow(t, 3) - (6 * Math.pow(t, 2)) + 3 * t);
+//     p2 = copyArr[2].mult(-3 * Math.pow(t, 3) + 3 * Math.pow(t, 2));
+//     p3 = copyArr[3].mult(Math.pow(t, 3));
+//     p0.add(p1);
+//     p0.add(p2);
+//     p0.add(p3);
+//     return p0;
+// }
+
+const deCasteljauAlgorithm = (Points, t) => {
+    let [P0, P1, P2, P3] = Points;
+    let A = linearInterpolation(P0, P1, t);
+    let B = linearInterpolation(P1, P2, t);
+    let C = linearInterpolation(P2, P3, t);
+    let D = linearInterpolation(A, B, t);
+    let E = linearInterpolation(B, C, t);
+    let P = linearInterpolation(D, E, t);
+    return {A, B, C, D, E, P};
+} 
+
+const vectLine = (P1, P2, color = "White") => {
+    fill(color)
+    line(P1.x, P1.y, P2.x, P2.y);
+    noFill();
+}
+
+const displayPoints = (Points, color = "White", onlyBorder = false) => {
+
+    if(!onlyBorder) fill(color);
+    else {
+        fill("black")
+        stroke(color);
+        strokeWeight(4);
+    }
+    Points.forEach(e => ellipse(e.x, e.y, r));
+    if(!onlyBorder) noFill();
+    strokeWeight(1);
+}
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let P0, P1, P2, P01, P12, Pmiddle;
-let tmpArr = [];
-let r = 10;
+let Points = [];
+let r = 15;
 let t = 0;
 let speed = 0.002;
 let display = false;
@@ -11,15 +10,10 @@ let vertexArray = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    P01 = createVector(0, 0);
-    P12 = createVector(0, 0);
-    Pmiddle = createVector(0, 0);
 }
 
 function draw() {
     background("black");
-
-    // display = keyIsPressed ? true : false;
     if (keyIsPressed) {
         switch (key) {
             case "d":
@@ -29,11 +23,8 @@ function draw() {
                 displayPoint = true;
                 break;
             case "s":
-                if (tmpArr.length == 3) {
+                if (Points.length === 4) {
                     start = true;
-                    P0 = tmpArr[0];
-                    P1 = tmpArr[1];
-                    P2 = tmpArr[2];
                 }
                 break;
             default:
@@ -50,38 +41,31 @@ function draw() {
     fill(c);
     stroke(255);
     if(start) {
-        P01 = linearInterpolation(P0, P1, t);
-        P12 = linearInterpolation(P1, P2, t);
-        Pmiddle = linearInterpolation(P01, P12, t);
-        vertexArray.push(Pmiddle);
-        if (display) {
-            ellipse(P0.x, P0.y, r);
-            ellipse(P1.x, P1.y, r);
-            ellipse(P2.x, P2.y, r);
-            line(P0.x, P0.y, P1.x, P1.y);
-            line(P1.x, P1.y, P2.x, P2.y);
-            ellipse(P01.x, P01.y, r);
-            ellipse(P12.x, P12.y, r);
-            line(P01.x, P01.y, P12.x, P12.y);
-            ellipse(Pmiddle.x, Pmiddle.y, r);
-        } else if (displayPoint) {
-            ellipse(P0.x, P0.y, r);
-            ellipse(P1.x, P1.y, r);
-            ellipse(P2.x, P2.y, r);
-        }
         noFill();
         beginShape();
         if (t < 1) t += speed;
-        vertexArray.forEach((e) => {
-            vertex(e.x, e.y);
+        const {A, B, C, D, E, P} = deCasteljauAlgorithm(Points, t);
+        vertexArray.push({A, B, C, D, E, P});
+        vertexArray.forEach(({P}) => {
+            vertex(P.x, P.y);
         });
         endShape();
+
+        if (display) {
+            vectLine(A, B);
+            vectLine(B, C);
+            vectLine(D, E);
+            vectLine(Points[0], Points[1]);
+            vectLine(Points[1], Points[2]);
+            vectLine(Points[2], Points[3]);
+            displayPoints([A, B, C, D, E, P, ...Points], "white", true);
+        } else if (displayPoint) {
+            displayPoints(Points, "white", true);
+        }
     }
 
     if (!start) {
-        tmpArr.forEach((e) => {
-            ellipse(e.x, e.y, r); 
-        });
+        displayPoints(Points, "white", true);
     }
  
 }
@@ -92,21 +76,9 @@ function windowResized() {
 
 function mouseClicked() {
     if (!start) {
-        tmpArr.push(createVector(mouseX, mouseY));
-        if (tmpArr.length > 3) tmpArr.shift();
-        console.log(tmpArr)
+        Points.push(createVector(mouseX, mouseY));
+        if (Points.length > 4) Points.shift();
+        console.log(Points)
     }
 }
 
-const linearInterpolation = (p0, p1, t) => {
-    let Px = (1-t) * p0.x + t * p1.x;
-    let Py = (1-t) * p0.y + t * p1.y;
-    // console.log(Math.floor(Px), Math.floor(Py), Math.floor(t));
-    return {x: Px, y: Py};
-}
-
-const middleOfLine = (P0, P1) => {
-    Px = (P0.x + P1.x) / 2; 
-    Py = (P0.y + P1.y) / 2; 
-    return {x: Px, y: Py};
-}
